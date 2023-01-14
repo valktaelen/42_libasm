bceeb9f93e693b73dc09801b5750ff4f30f06470
valktaelen <alix.artiges@hotmail.fr>
2023-01-14 21:21:28 +0100

    bonus remove if full stack

--- srcs/ft_list_remove_if_bonus.s
+++ srcs/ft_list_remove_if_bonus.s
@@ -9,8 +9,121 @@ extern _free
 _ft_list_remove_if:
 	cmp rdi, 0
 	je end
-	
+	cmp rsi, 0
+	je end
+	cmp rdx, 0
+	je end
+	cmp rcx, 0
+	je end
+	xor rax, rax
+	push rcx			; save free
+	push rdx			; save cmp
+	push rsi			; save ref
+	push rdi			; save begin
+	push rax			; push old struct
+	mov rax, [rdi]		; rax = struct*
+	push rax			; push current struct
+	jmp iter			; iterate
 
+iter:
+	pop r13				; current val
+	pop r12				; old val
+	pop r8				; begin
+	pop r9				; ref
+	pop r10				; cmp
+	pop r11				; free
+	cmp r13, 0			; struct* == NULL
+	je end					; return
+	push r11			; free
+	push r10			; cmp
+	push r9				; ref
+	push r8				; begin
+	push r12			; push old val
+	push r13			; push current val
+	mov rdi, r9			; param1 = ref
+	mov rsi, [r13]		; param2 = struct->data
+	call r10
+	cmp rax, 0
+	je remove
+	pop r13				; current val
+	pop r12				; old val
+	mov r12, r13
+	mov r13, [r13 + 8]
+	push r12			; push old val
+	push r13			; push current val
+	jmp iter
+
+remove:
+	pop r13				; current val
+	pop r12				; old val
+	pop r8				; begin
+	push r8				; begin
+	push r12			; push old val
+	push r13			; push current val
+	cmp r12, 0			; old == NULL
+	je removeStart			; rm at begin
+	push r13			; toDelete
+	mov r13, [r13 + 8]	; current = current->next
+	mov [r12 + 8], r13	; old->next = current
+	jmp removeNode
+
+;	Entry:
+;	Stack		Type		Use
+;	TOP
+;	current		node*
+;	old			node*
+;	END
+;	Exit:
+;	Stack		Type		Use
+;	TOP
+;	toDelete	node*		Create
+;	current		node*		Modify
+;	old			node*		N
+;	END
+removeStart:
+	pop r13				; current
+	mov rdi, r13		; toDelete
+	mov r13, [r13 + 8]	; next
+	mov [r8], r13		; start = current
+	push r13			; new current
+	push rdi			; toDelete
+	jmp removeNode
+
+
+;	Entry:
+;	Stack		Type		Use
+;	TOP
+;	toDelete	node*		Y
+;	current		node*		N
+;	old			node*		N
+;	END
+;	Exit:
+;	Stack		Type		Use
+;	TOP
+;	current		node*		N
+;	old			node*		N
+;	END
+
+removeNode:
+	pop rdi				; get	toDelete
+	pop r13				; current val
+	pop r12				; old val
+	pop r8				; begin
+	pop r9				; ref
+	pop r10				; cmp
+	pop r11				; free
+	push r11			; free
+	push r10			; cmp
+	push r9				; ref
+	push r8				; begin
+	push r12			; push old val
+	push r13			; push current val
+	push rdi			; save	toDelete
+	mov rdi, [rdi]		; toDelete.data
+	call r11			; free(toDelete.data)
+	pop rdi				; toDelete
+	;call _free			; free(rdi)				Why !!!! free data but not node, why not !!!
+	jmp iter
 
 end:
 	ret
