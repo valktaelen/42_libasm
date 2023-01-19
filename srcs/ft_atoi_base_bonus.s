f583eaab330cbe3552bd81f04444736dae2afec7
valktaelen <alix.artiges@hotmail.fr>
2023-01-19 16:41:46 +0100

    finish atoi

--- srcs/ft_atoi_base_bonus.s
+++ srcs/ft_atoi_base_bonus.s
@@ -5,186 +5,200 @@ extern	_ft_strlen
 ; int	ft_atoi_base(char const *rdi, char const *rsi);
 ;								num				base
 
+; TOP number, base, dimension, value, sign
+
 _ft_atoi_base:
 	cmp rdi, 0
 	je return
 	cmp rsi, 0
 	je return
-	push rsi
-	push rdi			; # TOP #  number (string) | base (string)
+	push rdi
+	push rsi			; TOP base, num
 	mov rdi, rsi
-	call check_base		; check_base(base)
-	mov rdi, rax
-	cmp rax, 0
-	jne convert
-	pop rdi
+	xor rsi, rsi
+	call check_base		; check_base(base) -> dimension
+	cmp rax, 2
+	jl return_error
+	mov rcx, rax
 	pop rsi
-	jmp return
-
-convert:
-	pop rdi 			; number (string)
-	pop rcx				; base (string)
-	mov rsi, 1
-	push rsi			; sign
-	push rax			; baseDim
-	xor rax, rax
-	push rax			; # TOP #  number (string) | base (string) | number | base (dim) | sign
-	push rcx			; base
-	push rdi
-	jmp begin
-
-begin:
 	pop rdi
-	push rdi
-	mov sil, BYTE [rdi]
-	cmp sil, 0
-	je end_convert
-	cmp sil, 43					; '+'
-	je begin
-	cmp sil, 45					; '-'
-	je change_sign
-	cmp sil, 32					; ' '
-	je convert_next
-	cmp sil, 9					; '\t'
-	je convert_next
-	cmp sil, 10					; '\n'
-	je convert_next
-	cmp sil, 13					; '\r'
-	je convert_next
-	cmp sil, 11					; '\v'
-	je convert_next
-	cmp sil, 12					; '\f'
-	je convert_next
-	jmp convert_iter
+	mov rax, 1			; sign
+	push rax
+	xor rax, rax		; value
+	mov rax, 0
+	push rax
+	push rcx
+	push rsi
+	push rdi			; TOP number, base, dimension, value, sign
+	jmp whitespace
 
-convert_next:
+whitespace:
+	xor rsi, rsi
+	pop rdi
+	mov r15b, BYTE [rdi]
+	push rdi
+	cmp r15b, 0
+	je end
+	cmp r15b, 32					; ' '
+	je whitespace_inc
+	cmp r15b, 9					; '\t'
+	je whitespace_inc
+	cmp r15b, 10					; '\n'
+	je whitespace_inc
+	cmp r15b, 13					; '\r'
+	je whitespace_inc
+	cmp r15b, 11					; '\v'
+	je whitespace_inc
+	cmp r15b, 12					; '\f'
+	je whitespace_inc
+	jmp sign
+
+whitespace_inc:
 	pop rdi
 	inc rdi
 	push rdi
-	jmp begin
+	jmp whitespace
 
-change_sign:	; # TOP #  number (string) | base (string) | number | base (dim) | sign
+sign:
+	xor rsi, rsi
+	pop rdi
+	mov r15b, BYTE [rdi]
+	push rdi
+	cmp r15b, 0
+	je end
+	cmp r15b, 43					; '+'
+	je sign_inc
+	cmp r15b, 45					; '-'
+	je change_sign
+	jmp conv_iter
+
+sign_inc:
+	pop rdi
+	inc rdi
+	push rdi
+	jmp sign
+
+change_sign:			; TOP number, base, dimension, value, sign
+	pop rdi
+	pop rsi
+	pop rdx
+	pop r11
+	pop rax
+	neg eax
+	push rax
+	push r11
+	push rdx
+	push rsi
+	push rdi
+	jmp sign_inc
+
+conv_iter:				; TOP number, base, dimension, value, sign
+	xor rsi, rsi
+	pop rdi
+	mov r15b, BYTE [rdi]
+	inc rdi
+	push rdi
+	cmp r15b, 0
+	je end
+	pop rdx
+	pop rdi
+	push rdi
+	push rdx
+	call digit
+	cmp rax, -1
+	je end
 	pop r11
 	pop r12
 	pop r13
 	pop r14
-	pop r15
-	inc r11
-	neg r15
-	push r15
+	imul r14d, r13d
+	add r14, rax
 	push r14
 	push r13
 	push r12
 	push r11
-	jmp begin
+	jmp conv_iter
 
-convert_iter:	; # TOP #  number (string) | base (string) | number | base (dim) | sign
-	pop rcx				; number (string)
-	pop rdi				; base (string)
-	mov sil, BYTE [rcx]	; digit
-	inc rcx
-	push rdi
-	push rcx
-	call get_pos_base	; get_pos_base(base, val (sil))
-	cmp rax, -1
-	je end_convert
-	mov r12, rax
-	pop rcx				; number (string)
-	pop rdi				; base (string)
-	pop rax				; number
-	pop rsi				; base
-	imul rax, rsi		; n * base
-	add rax, r11		; n + i
-	push rsi
-	push rax
-	push rdi
-	inc rcx
-	push rcx
-	jmp convert_iter
-
-end_convert:
+end:					; TOP number, base, dimension, value, sign
+	pop rax
 	pop rax
 	pop rax
 	pop rax
 	pop rdi
-	pop rdi
-	;imul rax, rdi
+	imul eax, edi
+	ret
+
+return_error:
+	pop rax
+	pop rax
+	xor rax, rax
 	ret
 
 return:
 	ret
 
-; Pos in base
-
-get_pos_base: ; get_pos_base(base, val (sil))
+digit:			; rdi (base), r15 [r15b] (digit)
 	xor rax, rax
-	jmp get_pos_base_iter
+	xor rdx, rdx
+	jmp digit_iter
 
-get_pos_base_iter:	; rdi base, sil val
-	cmp BYTE [rdi + rax], 0
-	je get_pos_base_error
-	cmp BYTE [rdi + rax], sil
+digit_iter:
+	mov r14b, BYTE [rdi + rax]
+	cmp r14b, 0
+	je return_digit_error
+	cmp r14b, r15b
 	je return
 	inc rax
-	jmp get_pos_base_iter
+	jmp digit_iter
 
-get_pos_base_error:
+return_digit_error:
 	mov rax, -1
 	ret
 
-; Check base
-
-check_base:	; check_base(base)
+check_base:	; rdi base
 	xor rax, rax
-	jmp iter_base
+	jmp check_base_iter
 
-iter_base:
-	mov sil, BYTE [rdi + rax]
-	cmp sil, 0
-	je return_base
-	cmp sil, 43					; '+'
+check_base_iter:
+	xor rsi, rsi
+	mov r15b, BYTE [rdi + rax]
+	cmp r15b, 0
+	je return
+	cmp r15b, 43					; '+'
 	je return_base_error
-	cmp sil, 45					; '-'
+	cmp r15b, 45					; '-'
 	je return_base_error
-	cmp sil, 32					; ' '
+	cmp r15b, 32					; ' '
 	je return_base_error
-	cmp sil, 9					; '\t'
+	cmp r15b, 9					; '\t'
 	je return_base_error
-	cmp sil, 10					; '\n'
+	cmp r15b, 10					; '\n'
 	je return_base_error
-	cmp sil, 13					; '\r'
+	cmp r15b, 13					; '\r'
 	je return_base_error
-	cmp sil, 11					; '\v'
+	cmp r15b, 11					; '\v'
 	je return_base_error
-	cmp sil, 12					; '\f'
+	cmp r15b, 12					; '\f'
 	je return_base_error
-	mov rsi, rax
+	inc rax
 	push rax
-	mov rdx, rax				; rdx = i
-	call check_base_unique
+	mov rdx, rax
+	call check_base_double
+	pop rsi
 	cmp rax, 0
-	pop rax
+	je return
+	mov rax, rsi
+	jmp check_base_iter
+
+check_base_double:
+	mov rax, 1
+	cmp BYTE [rdi + rdx], 0
+	je return
+	cmp r15b, BYTE [rdi + rdx]
 	je return_base_error
-	inc rax
-	jmp iter_base
+	inc rdx
+	jmp check_base_double
 
-check_base_unique:
-	mov rax, rdx
-	jmp check_base_unique_iter
-
-check_base_unique_iter: ; rax i, rdi base, sil char
-	inc rax
-	mov bl, BYTE [rdi + rax]
-	cmp bl, 0
-	je return_base
-	cmp bl, sil
-	je return_base_error
-	jmp check_base_unique_iter
-
-return_base:
-	cmp rax, 2
-	jl return_base_error
-	ret
 
 return_base_error:
 	xor rax, rax
